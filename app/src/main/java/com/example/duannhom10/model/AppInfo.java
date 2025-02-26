8a986bf1dea8c7023ec8612f18588e12385e78f8
Cuong <cuong.trantrong@mbs.com.vn>
2025-02-26 15:08:43 +0700

    Update additional AppInfo sprint 5.2

---
+++ app/src/main/java/com/example/duannhom10/model/AppInfo.java
@@ -0,0 +1,8 @@
+package com.example.duannhom10.model;
+
+public class AppInfo {
+    public static final int APP_ID = 2553;
+    public static final String MAC_KEY = "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL";
+    public static final String URL_CREATE_ORDER = "https://sb-openapi.zalopay.vn/v2/create";
+}
+
